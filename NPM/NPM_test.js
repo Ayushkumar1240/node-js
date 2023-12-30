e17338e67e418f93953a94a740596702791de4cd
Ayush Kumar <ayushkumar4975@gmail.com>
2023-12-30 21:01:28 +0530

    commiting

---
+++ NPM/NPM_test.js
@@ -0,0 +1,12 @@
+const figlet=require('figlet')
+
+figlet('Hello World!!',(err,data)=>{
+    if(err){
+        console.log("Something Went Wrong...");
+        console.log(err);
+        return;
+    }
+    else{
+        console.log(data)
+    }
+});
---
+++ test.js
@@ -0,0 +1,15 @@
+//console.log("Scalar is Awesome");
+
+
+//console.log(windows);
+
+
+// console.log(global);
+
+
+const calculator=require('./calculator')
+
+calculator.addition(1,2);
+calculator.subtraction(3,4);
+calculator.multiplication(4,5);
+calculator.division(4,5);
